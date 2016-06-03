53f6f5fc23811fac239ebd8c6e251c844dc29a61
Metod Medja <metod@medja.net>
2016-06-03 16:52:41 +0000

    posodobljene rešitve

--- DN-2/skupina-1/lazja.sh
+++ DN-2/skupina-1/lazja.sh
@@ -2,14 +2,28 @@
 
 WORD="[[:alpha:]]*"
 
-min_count=1
+# Privzeto najmanjse stevilo omejitev je nastavljeno na 0.
+# V tem primeru, je omejitev na pojavitvah besede izklopljena.
+min_count=0
 word_length=0
+
+# Primerjalni operator pri preverjanju pojavitev besed.
+# Privzeto se ne izpisejo besede, ki se ne pojavijo dovoljkrat.
+omejitev="-lt"
 
 while [ $# -gt 0 ]; do
     if [ "${1:0:1}" = "-" ]; then
         case "$1" in
             -n)
-                min_count=$2
+                # V primeru, da stikalo -n dobi število s pomišljajem,
+                # je potrebno spremeniti omejitev na številu pojativev
+                # in izluščiti ta pomišljaj iz vrednosti stikala.
+                if [ "${2:0:1}" = "-" ]; then
+                    min_count="${2:1}"
+                    omejitev="-gt"
+                else
+                    min_count="$2"
+                fi
                 ;;
             -l)
                 word_length=$2
@@ -41,7 +55,9 @@ parse() {
 
 prepare() {
     while read count word; do
-        if [ $count -lt $min_count ]; then
+        # Dodan je pogoj za izklop omejitve in spremenjen operator.
+        # Ker je ta v resnici argument ukaza test, se lahko tu uporabi spremenljivka.
+        if [ $min_count -gt 0 ] && [ $count $omejitev $min_count ]; then
             continue
         fi
         
